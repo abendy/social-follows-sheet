e548b86be2dbe79b05509501065315e2247ecd84
abendy <1480258+abendy@users.noreply.github.com>
2025-05-11 14:15:52 -0400

    dev configs

---
+++ .prettierrc.js
@@ -0,0 +1,28 @@
+'use strict'
+
+/** @type {import("prettier").Config} */
+module.exports = {
+    arrowParens: 'avoid',
+    bracketSameLine: false,
+    bracketSpacing: true,
+    commaDangle: 'always-multiline',
+    commaSpacing: true,
+    editorconfig: true,
+    endOfLine: 'lf',
+    flattenTernaries: false, // true ?
+    generatorStarSpacing: true,
+    jsxBracketSameLine: false,
+    jsxSingleQuote: false,
+    keySpacing: true,
+    offsetTernaryExpressions: true,
+    printWidth: 80,
+    quoteProps: 'consistent',
+    quotes: true,
+    semi: false,
+    singleQuote: true,
+    spaceBeforeFunctionParen: true,
+    tabWidth: 4,
+    trailingComma: 'all',
+    useTabs: false,
+    yieldStarSpacing: true,
+}
